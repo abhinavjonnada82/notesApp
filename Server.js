5163da9766bf883c475c137bb07bb01fd564dfb8
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2019-03-12 03:53:45 -0500

    make it better

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ const admin = require('firebase-admin');
 const router = require('./api/MongoEP');
 const cors = require('cors');
 const app = express();
+const path = require('path')
 
 
 // Automatically allow cross-origin requests
@@ -66,6 +67,16 @@ mongoose
 app.use('/notes', router)
 
 
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+   // Set static folder
+   app.use(express.static('client/build'))
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   })
+}
 
 const port = process.env.PORT || 5000
 

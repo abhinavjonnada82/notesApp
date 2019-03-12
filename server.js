a569ebabffd15babaf2e82ad3afdcc2e303690b4
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2019-03-12 13:44:48 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const admin = require('firebase-admin');
 const router = require('./api/MongoEP');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 
@@ -65,7 +66,16 @@ mongoose
 // Use Mongoose Routes
 app.use('/notes', router)
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+   // Set static folder
+   app.use (express.static('client/build'))
 
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
+   })
+}
 
 const port = process.env.PORT || 5000
 
